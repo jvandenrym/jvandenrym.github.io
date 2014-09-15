a60c768c1665438c62ec505fa4ef15a7cec73ead
Johan Van den Rym <vandenrym.johan@gmail.com>
2014-09-15 03:26:12 +0200

    Added ruby config file

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'susy'
+require 'breakpoint'
+
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = '_sass'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/js'
+
+#sourcemaps
+sass_options = {:sourcemap => true}
+enable_sourcemaps = true
